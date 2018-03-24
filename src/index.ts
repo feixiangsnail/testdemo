bb8b18ec7ed9ecf69c47ce8e9b9d751c2f58c998
feixiangsnail <feixiangsnail@hotmail.com>
2018-03-24 19:13:45 +0800

    第一次搭建typescript脚手架

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+enum Choose{
+    Wife = 1,
+    Mother = 2
+}
+function question(choose:Choose): void {
+    console.log('which');
+    console.log('your choose:',choose,666);
+}
+question(Choose.Mother);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
