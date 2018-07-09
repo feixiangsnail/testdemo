f5a6ada1c897ab5ca423188761063042746c8128
feixiangsnail <feixiangsnail@users.noreply.github.com>
2018-07-09 19:46:54 +0800

    Update main.js

--- reacts/src/app/main.js
+++ reacts/src/app/main.js
@@ -37,3 +37,64 @@ class Square extends React.Component {
     <Square />,
     document.getElementById('app')
 )
+
+
+
+
+
+
+
+//----------------------
+import React from "react"
+import ReactDom from "react-dom"
+class Life extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      opacity: 1
+    }
+    this.distroyComponent = this.distroyComponent.bind(this)
+  }
+  distroyComponent(){
+    console.log("abcdestroied")
+    ReactDom.unmountComponentAtNode(document.getElementById('app'))
+  }
+  componentDidMount(){
+    let temp = 0.08;
+    this.intervalId = setInterval(()=>{
+      let {opacity} = this.state
+      opacity-=temp;
+      if (opacity<=0 ||opacity>=1){
+        temp = -temp;
+      }
+      
+      this.setState({opacity})
+
+    },200)
+   
+  }
+  componentWillMount(){
+    console.log(this.state)
+    console.log("willmounted")
+  }
+ componentWillUnmount(){
+   clearInterval(this.intervalId)
+ }
+  render() {
+    const { opacity } = this.state
+    return (
+      <div>
+        <h2 id="example" style={{opacity:opacity}}>
+            {this.props.msg}
+        </h2>
+        <button onClick={this.distroyComponent}>dianji</button>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<Life msg="tainanle"/>,document.getElementById("app"))
+
+
+
+
